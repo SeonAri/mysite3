0607ae73e3efc2ef23b020659116f91ec9e531c9
kickscar <kickscar@gmail.com>
2016-04-28 05:42:12 +0900

    UserDao 개비

--- src/main/java/com/estsoft/mysite/dao/UserDao.java
+++ src/main/java/com/estsoft/mysite/dao/UserDao.java
@@ -1,106 +1,34 @@
 package com.estsoft.mysite.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.estsoft.db.DBConnection;
 import com.estsoft.mysite.vo.UserVo;
 
 @Repository
 public class UserDao {
-	@Autowired
-	private DBConnection dbConnection;
 
 	@Autowired
 	private SqlSession sqlSession;
 
 	public UserVo get(String email) {
-		UserVo vo = sqlSession.selectOne("user.selectByEmail", email);
-		return vo;
+		return sqlSession.selectOne("user.selectByEmail", email );
 	}
 
 	public UserVo get(Long userNo) {
-		UserVo vo = sqlSession.selectOne("user.selectByNo", userNo);
-		return vo;
+		return sqlSession.selectOne("user.selectByNo", userNo);
 	}
 
-	public UserVo get(UserVo vo) {
-		UserVo userVo = sqlSession.selectOne("user.selectByEmailAndPassword", vo);
-		return userVo;
+	public UserVo get(UserVo userNo) {
+		return sqlSession.selectOne("user.selectByEmailAndPassword", userNo);
 	}
 
 	public void update(UserVo userVo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		try {
-			conn = dbConnection.getConnection();
-			if ("".equals(userVo.getPassword())) {
-				String sql = "UPDATE user SET  name=?, gender=? WHERE no = ?";
-				pstmt = conn.prepareStatement(sql);
-
-				pstmt.setString(1, userVo.getName());
-				pstmt.setString(2, userVo.getGender());
-				pstmt.setLong(3, userVo.getNo());
-			} else {
-				String sql = "UPDATE user SET  name=?, gender=?, passwd=password(?) WHERE no = ?";
-				pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, userVo.getName());
-				pstmt.setString(2, userVo.getGender());
-				pstmt.setString(3, userVo.getPassword());
-				pstmt.setLong(4, userVo.getNo());
-			}
-
-			pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		sqlSession.update( "user.update", userVo );
 	}
 
-	public void insert(UserVo vo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		try {
-			conn = dbConnection.getConnection();
-			String sql = "INSERT INTO user VALUES (null, ?, ?, password(?), ? )";
-
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getEmail());
-			pstmt.setString(3, vo.getPassword());
-			pstmt.setString(4, vo.getGender());
-
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+	public void insert(UserVo userVo) {
+		sqlSession.update( "user.update", userVo );
 	}
 }
